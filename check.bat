682d6015a686968965e8ebdf65f7264ce46f323c
Администратор <@localhost>
2021-08-07 11:06:25 +0300

    adsasd

--- check.bat
+++ check.bat
@@ -1,1 +1,1 @@
-C:\TOOLS\sonar-scanner\bin\sonar-scanner.bat -D"sonar.projectKey=bec66be73ecc9a06d6173aa606a7813945b10942" -D"sonar.sources=." -D"sonar.host.url=http://localhost:9000" -D"sonar.login=bec66be73ecc9a06d6173aa606a7813945b10942"
+sonar-scanner -D"sonar.projectKey=bec66be73ecc9a06d6173aa606a7813945b10942" -D"sonar.sources=." -D"sonar.host.url=http://localhost:9000" -D"sonar.login=bec66be73ecc9a06d6173aa606a7813945b10942"
